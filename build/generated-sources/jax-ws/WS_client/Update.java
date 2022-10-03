498f88003aa99b315c8eea6e5aa021034a5a1a88
S4VIT4R <99379984+S4VIT4R@users.noreply.github.com>
2022-10-02 22:14:20 -0500

    subiendo proyecto

---
+++ build/generated-sources/jax-ws/WS_client/Update.java
@@ -0,0 +1,152 @@
+
+package WS_client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para update complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="update"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="apellido" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="sexo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="edad" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "update", propOrder = {
+    "id",
+    "nombre",
+    "apellido",
+    "sexo",
+    "edad"
+})
+public class Update {
+
+    protected int id;
+    protected String nombre;
+    protected String apellido;
+    protected String sexo;
+    protected int edad;
+
+    /**
+     * Obtiene el valor de la propiedad id.
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Define el valor de la propiedad id.
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad nombre.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Define el valor de la propiedad nombre.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNombre(String value) {
+        this.nombre = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad apellido.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * Define el valor de la propiedad apellido.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setApellido(String value) {
+        this.apellido = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad sexo.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * Define el valor de la propiedad sexo.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSexo(String value) {
+        this.sexo = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad edad.
+     * 
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Define el valor de la propiedad edad.
+     * 
+     */
+    public void setEdad(int value) {
+        this.edad = value;
+    }
+
+}
